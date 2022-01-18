087780a7a64af28d56c0e2e2e2fba62ed141da29
staffsterr2000 <rosenkostas@gmail.com>
2022-01-18 16:24:19 +0200

    http basic security logic embedded

--- src/main/java/com/staffsterr2000/bankapi/controller/ApplicationController.java
+++ src/main/java/com/staffsterr2000/bankapi/controller/ApplicationController.java
@@ -4,6 +4,7 @@ import com.staffsterr2000.bankapi.model.Application;
 import com.staffsterr2000.bankapi.service.ApplicationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,19 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/applications")
 @AllArgsConstructor
+@RequestMapping("/applications")
 public class ApplicationController {
 
     private final ApplicationService applicationService;
 
     @PostMapping
-//    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Application> save(@RequestBody Application application) {
         return ResponseEntity.ok(applicationService.save(application));
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public String string(@PathVariable String id) {
         return String.format("success for string %s", id);
     }
